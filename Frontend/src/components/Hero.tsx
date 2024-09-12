2355a30f8f0ed8501b9de80547e6bbdb45f2e4f2
Brendan <cleavermedia@outlook.com>
2024-09-13 00:21:13 +0200

    added auth & auth tests & initial layout

---
+++ Frontend/src/components/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className="bg-blue-800 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <h1 className="text-5xl text-white font-bold">Getting a quote has never been easier</h1>
+        <p className="text-2xl text-white">
+          Search low prices on Freight Services now.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
